9d245c16a6d3bd1426ded0c9bba26abc260a0fc6
Anish <anishkbansal2007@gmail.com>
2024-05-10 21:56:53 +0530

    lect8

--- Python/0.7.trial.py
+++
@@ -1,8 +0,0 @@
-trait= [" big ", " small ", " cute ", " attractive ", " fat "]
-noun = [" dog ", " girl ", " boy ", " dinasaour "]
-verb = [" danced ", " sleeps ", " jumps ", " plays ", " sings "]
-
-for i in trait:
-    for j in noun:
-        for k in verb:
-            print(i+j+k)
