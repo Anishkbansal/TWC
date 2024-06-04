b50cb8c71b8ad576ec7501c0797ca61a857a8a39
Anish <anishkbansal2007@gmail.com>
2024-06-04 11:04:17 +0530

    lect 25

--- Python/24 trial.py
+++ Python/24 trial.py
@@ -1,17 +1,27 @@
-# imports
-import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
 
-url = "https://weather.com/weather/today/l/31.1471,75.3412?unit=m"
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html , 'html.parser')     # parsing means analiseing
-# it makes messy stuff into beautiful
+class Student:  # class is keyword that makes classes
+    def __init__(self , name , ClassName , rollNo):    # this is method that initialise the class when creating object. it has special name __init__
+        # the parameter self means the object itself  example if you create object student1.
+        
+        self.name = name      # this actually says student1.name = name 
+        # its like for the object (self) create a variable name. anf give it value of name provided as function argument
+        self.ClassName = ClassName
+        self.rollNo = rollNo
+    
+    def student_info(self):   # this is a method
+        print(f"Class : {self.ClassName} , Name : {self.name} , rollNo : {self.rollNo}")
 
-# retreive all anchor tags  (anchor tags hold the link in a page)
-tags = soup('div', class_='CurrentConditions--tempHiLoValue--3T1DG')   # this gives list of all the tags in the document.
-for tag in tags:
-    print(tag.getText())   # href is the link. and text is text
+# whenever you create an object, python runs the init function automatically, so you also have to define its parameters/arguments
+student1 = Student("Rohit" , "Science" , 21)   # student1 is an object/instance
+# you dont have to define self argument because it is defined as student1 automatically
+student1.student_info()
+print(type(student1))
 
-# an anchor tag in html looks like This     <a href="https://hostname.com ">some text </a>
+student2 = Student("Anish" , "Science" , 18)  # another object that belongs to class Student
+student2.student_info()
+print(dir(student2))
+# you can imagine this syntax as   Student.student_info(student2)  
 
 
+print(type(student1.name))
+print(type(student1.student_info))
